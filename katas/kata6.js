39924c7f074e04e2eb62c26bf4abe4daa110eb43
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-01-13 02:43:53 +0000

    starting

---
+++ katas/kata6.js
@@ -0,0 +1,12 @@
+const repeatNumbers = function(data) {
+  // Put your solution here
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+
+/*Create a function named repeatNumbers that will return a string 
+with each of the given values repeated the appropriate number of times,
+if there are multiple sets of values each set should be separated by a comma. 
+If there is only one set of values then you should omit the comma. */
