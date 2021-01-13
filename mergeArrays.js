12eb2718d2ebb638a2335cff72ad7e4bc02f56ef
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-01-13 02:36:59 +0000

    solved

--- mergeArrays.js
+++ mergeArrays.js
@@ -1,3 +1,12 @@
+const merge = function (first, second) {
+  const resultedArray = first;
+  for (let value of second) {
+    resultedArray.push(value);
+  }
+  return resultedArray.sort();
+}
+
+
 console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
 console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
 console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
