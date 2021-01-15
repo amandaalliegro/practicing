a06cbf4cb948be92a492e3ab1c822675bb7bb937
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-01-15 02:33:37 +0000

    kata7 completed

---
+++ katas/kata7.js
@@ -0,0 +1,18 @@
+const camelCase = function(input) {
+  // Your code here
+  let newStr = "";
+  for (let i = 0; i < input.length; i++) {
+
+    if (input[i] === " "){
+      i++;
+      newStr += input[i].toUpperCase();
+    } else {
+      newStr += input[i];
+    }
+  }
+  return newStr;
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
