83a1bcb989f873976a754524d1e38a76b11613cd
amandaalliegro <amanda.m.arnaut@gmail.com>
2021-01-16 01:06:31 +0000

    adding some comments to guide

--- katas/kata9.js
+++ katas/kata9.js
@@ -1,24 +1,29 @@
 // Use the value below whenever you need the value of Pi
 const PI = 3.14159 ;
 
+//sphereVolume()will calculate the volume of a sphere given a radius
 const sphereVolume = function (radius) {
   // Code here!
 }
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
+//coneVolume() will calculate the volume of a cone given a radius and a height
 const coneVolume = function (radius, height) {
   // And here!
 }
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
+//prismVolume() will calculate the volume of a prism given a height, a width, and a depth
 const prismVolume = function (height, width, depth) {
   // Probably here too!
 }
 
 console.log(prismVolume(3, 4, 5) === 60);
 
+/*The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single attraction. 
+The totalVolume function should use the previous three functions to calculate the total volume of an attraction. */
 const totalVolume = function (solids) {
   // Code here? Yup!
 }
